e125425ed5700205595cd987b52bd80338d37bae
WildanFajarMaulana <wildanfajar71@gmail.com>
2020-09-03 15:47:45 -1200

    tugas ta

---
+++ js/info.js
@@ -0,0 +1,16 @@
+// navbar
+let navbar = document.querySelector(".navbar");
+
+window.onscroll = function(){
+	scrollFunction();
+}
+
+
+function scrollFunction(){
+	if(document.body.scrollTop>20 || document.documentElement.scrollTop >20){
+		navbar.classList.add("navbar-fixed");
+	}
+	else{
+		navbar.classList.remove("navbar-fixed");
+	}
+}
